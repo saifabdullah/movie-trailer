0bbca9cc02b034d280047ad60278cd4311e88a64
saif abdullah <saifab@gmail.com>
2017-02-11 10:08:39 +0600

    Update media.py

--- media.py
+++ media.py
@@ -2,34 +2,34 @@
 
 import webbrowser
 
+
 class Video():
-    def __init__(self,title,duration):
+
+    def __init__(self, title, duration):
         self.title = title
         self.duration = (duration)
 
-    
-class Movie(Video):
 
-    valid_ratings = ["G", "PG", "PG-13", "R"]
+class Movie(Video):         # The main class for movie-trailer website
 
-    def  __init__(self, title, duration, movie_storyline, poster_image_url,
+    valid_ratings = ["G", "PG", "PG-13", "R"]        # Global variable
+
+    def __init__(self, title, duration, movie_storyline, poster_image_url,
                  trailer_youtube_url):
-        Video.__init__(self,title,duration)
+        Video.__init__(self, title, duration)
         self.storyline = movie_storyline
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
 
-
     def show_trailer(self):
 
         webbrowser.open(self.trailer_youtube_url)
-        
+
+
 class Tvshow(Video):
 
-
-    def __init__(self,title, duration, season, episode, tv_station):
-        Video.__init__(self,title,duration)
+    def __init__(self, title, duration, season, episode, tv_station):
+        Video.__init__(self, title, duration)
         self.season = season
         self.episode = episode
         self.tv_station = tv_station
-
